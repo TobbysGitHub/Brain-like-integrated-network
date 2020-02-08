a675e3cf676c9be84ff77fa422b4da0ad9ccd721
TobbysGitHub <15652969420@163.com>
2020-02-08 16:17:24 +0800

    model.py

---
+++ model.py
@@ -0,0 +1,46 @@
+import torch
+from torch import nn
+
+
+class Memory:
+    pass
+
+
+class EncodeNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
+class AggregateNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
+class CortexUnit(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
+class CortexLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
+class PrimaryLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
